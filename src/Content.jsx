033edec94a0a01e6433d070ed894d4a9baee0b07
JRisJunior <jeffmrubin@gmail.com>
2023-02-01 12:42:32 -0600

    start resolving merge conflicts

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,11 +2,23 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { StudentsIndex } from "./StudentsIndex";
 
-
 export function Content() {
+
+  const [students, setStudents] = useState([]);
+
+  const handleIndexStudents = () => {
+    console.log("handleIndexStudents");
+    axios.get("http://localhost:3000/students.json").then((response) => {
+      console.log(response.data);
+      setStudents(response.data);
+    });
+  };
+
+useEffect(handleIndexStudents, []);
+
   return (
     <div>
-      <StudentsIndex />
+      <StudentsIndex students={students} />
     </div>
   );
 }
--- src/StudentsIndex.jsx
+++ src/StudentsIndex.jsx
@@ -1,7 +1,12 @@
-export function StudentsIndex() {
+export function StudentsIndex(props) {
   return (
     <div>
       <h1>All students</h1>
+      {props.students.map((student) => (
+        <div key={student.id}>
+          <h2>{student.first_name}</h2>
+        </div>
+      ))};
     </div>
   );
 }
