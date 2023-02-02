e767e44a19c4b10e00aac3fa247b590496d5a7e9
johnryanmal <johnryanmal@gmail.com>
2023-02-02 09:52:41 -0800

    make resume index (home page)

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,44 +1,8 @@
 import { Home } from "./Home";
 import { Resume } from "./Resume";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 export function Content() {
-  const [students, setStudents] = useState([]);
-  const [educations, setEducations] = useState([]);
-  const [experiences, setExperiences] = useState([]);
-  const [capstones, setCapstones] = useState([]);
-
-  const handleIndexStudent = () => {
-    axios.get("http://localhost:3000/students.json").then((response) => {
-      setStudents(response.data);
-    });
-  };
-
-  const handleIndexEducation = () => {
-    axios.get("http://localhost:3000/educations.json").then((response) => {
-      setEducations(response.data);
-    });
-  };
-
-  const handleIndexExperience = () => {
-    axios.get("http://localhost:3000/experiences.json").then((response) => {
-      setExperiences(response.data);
-    });
-  };
-
-  const handleIndexCapstones = () => {
-    axios.get("http://localhost:3000/capstones.json").then((response) => {
-      setCapstones(response.data);
-    });
-  };
-
-  useEffect(handleIndexStudent, []);
-  useEffect(handleIndexEducation, []);
-  useEffect(handleIndexExperience, []);
-  useEffect(handleIndexCapstones, []);
-
   return (
     <div>
       <h1>
@@ -46,8 +10,8 @@ export function Content() {
       </h1>
 
       <Routes>
-        <Route path="/" element={<Home students={students} />} />
-        <Route
+        <Route path="/" element={<Home/>} />
+        {/* <Route
           path="/resume"
           element={
             <Resume
@@ -57,7 +21,7 @@ export function Content() {
               capstones={capstones}
             />
           }
-        />
+        /> */}
       </Routes>
     </div>
   );
